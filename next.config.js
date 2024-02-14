58159b2aefd9db138af2a8dea31bc59fe6c0092d
phil <phil@poppins.agency>
2024-02-14 11:41:34 +0000

    Add env.mjs file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.mjs");
 
-module.exports = nextConfig
+import withBundleAnalyzer from "@next/bundle-analyzer";
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  images: {
+    domains: ["a.storyblok.com"],
+  },
+};
+export default bundleAnalyzer(config);
