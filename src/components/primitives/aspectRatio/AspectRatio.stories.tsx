c77a16930e9260ea187ca0a1ead0bec10da9369a
phil <phil@poppins.agency>
2024-02-15 13:52:54 +0000

    Fix build commands for sb

--- src/components/primitives/aspectRatio/AspectRatio.stories.tsx
+++ src/components/primitives/aspectRatio/AspectRatio.stories.tsx
@@ -11,14 +11,40 @@ type Story = StoryObj<typeof AspectRatio>;
 
 export const Basic: Story = {
   render: () => (
-    <div className="w-[450px]">
-      <AspectRatio ratio={16 / 9}>
-        <Image
-          src="https://source.unsplash.com/random"
-          alt="Image"
-          className="rounded-md object-cover"
-        />
-      </AspectRatio>
+    <div className="w-[450px] space-y-10">
+      <div className="spacep-y-2">
+        <h3 className="underline font-bold text-xl">16/9</h3>
+        <AspectRatio ratio={16 / 9} className="relative">
+          <Image
+            src="https://source.unsplash.com/random"
+            alt="Image"
+            fill
+            className="rounded-md object-cover"
+          />
+        </AspectRatio>
+      </div>
+      <div className="spacep-y-2">
+        <h3 className="underline font-bold text-xl">1/1</h3>
+        <AspectRatio ratio={1 / 1} className="relative">
+          <Image
+            src="https://source.unsplash.com/random"
+            alt="Image"
+            fill
+            className="rounded-md object-cover"
+          />
+        </AspectRatio>
+      </div>
+      <div className="spacep-y-2">
+        <h3 className="underline font-bold text-xl">4/3</h3>
+        <AspectRatio ratio={4 / 3} className="relative">
+          <Image
+            src="https://source.unsplash.com/random"
+            alt="Image"
+            fill
+            className="rounded-md object-cover"
+          />
+        </AspectRatio>
+      </div>
     </div>
   ),
 };
